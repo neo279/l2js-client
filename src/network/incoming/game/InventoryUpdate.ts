9e7d161fa0116b36fda718b5526603a1bfaf08a6
filip <62439459+neo279@users.noreply.github.com>
2022-02-16 23:09:50 +0100

    fix: item update
    - remove item from inventory that was deleted
    - replace item in inventory when updated (correctly match by object id instead of id)

--- src/network/incoming/game/InventoryUpdate.ts
+++ src/network/incoming/game/InventoryUpdate.ts
@@ -3,6 +3,7 @@ import GameClientPacket from "./GameClientPacket";
 
 export default class InventoryUpdate extends GameClientPacket {
   Items: L2Item[] = [];
+  ItemsToRemove: L2Item[] = [];
 
   // @Override
   readImpl(): boolean {
@@ -11,7 +12,12 @@ export default class InventoryUpdate extends GameClientPacket {
     const _size = this.readH();
     for (let i = 0; i < _size; i++) {
       const _updateType = this.readH(); // Update type : 01-add, 02-modify, 03-remove
-      this.Items.push(this.readItem());
+      const item = this.readItem();
+      if (_updateType === 3) {
+        this.ItemsToRemove.push(item);
+      } else {
+        this.Items.push(item);
+      }
     }
 
     return true;
--- src/network/mutators/game/InventoryUpdateMutator.ts
+++ src/network/mutators/game/InventoryUpdateMutator.ts
@@ -5,12 +5,16 @@ import InventoryUpdate from "../../incoming/game/InventoryUpdate";
 export default class InventoryUpdateMutator extends IMMOClientMutator<GameClient, InventoryUpdate> {
   update(packet: InventoryUpdate): void {
     packet.Items.forEach((item) => {
-      const currentItem = this.Client.InventoryItems.getEntryById(item.Id);
+      const currentItem = this.Client.InventoryItems.getEntryByObjectId(item.ObjectId);
       if (currentItem) {
         this.Client.InventoryItems.delete(currentItem);
       }
 
       this.Client.InventoryItems.add(item);
     });
+
+    for (const item of packet.ItemsToRemove) {
+      this.Client.InventoryItems.removeByObjectId(item.ObjectId);
+    }
   }
 }
